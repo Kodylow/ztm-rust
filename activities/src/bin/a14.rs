f29f8dc6bccd3f6708cedbb93220746b5c3fb3aa
Kody Low <kodylow7@gmail.com>
2022-03-01 18:48:16 -0800

    strings and vectors exercises

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,39 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    favorite_color: String
+}
+fn main() {
+    let a = Person {
+        age: 1,
+        name: String::from("Kody"),
+        favorite_color: String::from("Orange")
+    };
+
+    let b = Person {
+        age: 2,
+        name: String::from("Ali"),
+        favorite_color: String::from("Pink")
+    };
+
+    let c = Person {
+        age: 1,
+        name: String::from("Rory"),
+        favorite_color: String::from("Green")
+    };
+
+    let people = vec![a, b, c];
+
+    for p in people {
+        if p.name == "Kody" {
+            println!("{:?}", p.name);
+            println!("{:?}", p.age);
+            println!("{:?}", p.favorite_color);
+        } else {
+            println!("Not Kody");
+        }
+    }
+}
